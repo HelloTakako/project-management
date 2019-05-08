b41991f0b3cce73a9cce24c58b2b7051faf9ddc9
HelloTakako <24834291+HelloTakako@users.noreply.github.com>
2019-05-08 14:23:30 -0700

    test gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,27 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-}
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var sourcemaps = require('gulp-sourcemaps');
+var cmq = require('gulp-combine-media-queries');//@media
 
-exports.default = defaultTask
+gulp.task('sass', function () {
+    gulp.src('sass/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(plumber())
+        .pipe(sass({ style: 'expanded' }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('cmq', function () {
+    gulp.src('css/*.css')
+        .pipe(cmq({
+            log: false
+        }))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('src/css/*.sass', ['sass']);
+});
+gulp.task('default', ['sass', 'cmq']);
