1655afcf87a5f09e30ae890f886eff3be884cb18
HelloTakako <24834291+HelloTakako@users.noreply.github.com>
2019-05-16 00:01:21 -0700

    change style

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 'use strict';
 
 window.onload = function() {
-    const slides = document.getElementById("home-slide");    
+    const slides = document.getElementById("home-slide");
+
+    function slider(){
+        console.log(slides.children);    
+    }
+
+    // setInterval(slider, 3000);
 }
